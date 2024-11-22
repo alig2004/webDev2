f69cb7e50409547464bf3894d9bb86046e82a27a
alig2004 <151762847+alig2004@users.noreply.github.com>
2024-11-22 22:31:26 +0000

    Delete database.php
    security issue

--- database.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "library";
-
-    //create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-?>
