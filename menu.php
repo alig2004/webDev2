334b189dcfe424c6e68bf4f9b6404d801f47fddf
alig2004 <151762847+alig2004@users.noreply.github.com>
2024-11-26 22:32:33 +0000

    Update menu.php
    adding form with drop down menu of book categories

--- menu.php
+++ menu.php
@@ -16,17 +16,43 @@
         <title>Alexandria Library</title>
     </head>
     <body>
-        
+        <a href="http://localhost/WebD/project/reserve.php">Reserve</a>
+        <h2>Search:</h2>
+        <form method="post">
+            <label>Title:</label><br>
+            <input type="text" name="title"><br>
+            <label>Author:</label><br>
+            <input type="text" name="auth"><br><br>
+            <select name="category" id="category">
+                <?php
+                    $sql = "SELECT CategoryDesc FROM category";
+                    $result = $conn->query($sql);
+
+                    if ($result->num_rows > 0) 
+                    {
+                        $cID = 1;
+                        while($row = $result->fetch_assoc()) 
+                        {
+                            echo '<option value="' . $cID . '">' . $row["CategoryDesc"] . '</option>';
+                            $cID++;
+                        }
+                    }
+                ?>
+            </select>
+            <input type="submit" value="Search"/>
+        </form>
     </body>
     <?php
-        /*
-        - make a search bar
-        - access books via searchbar (select statements with LIKE??)
-        - access categories via dropdown menu
-        - display books matching search (max 5 at a time)
-        */
+            /*
+            - make a search bar
+            - access books via searchbar (select statements with LIKE??)
+            - access categories via dropdown menu
+            - display books matching search (max 5 at a time)
+            - button to reserve each book
+            - logout
+            */
 
-        //closing the connection
-        $conn->close();
+            //closing the connection
+            $conn->close();
     ?>
 </html>
