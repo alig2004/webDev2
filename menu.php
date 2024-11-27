45b569a0fe6792333f664a81122e17d0ae02c5f6
alig2004 <151762847+alig2004@users.noreply.github.com>
2024-11-27 18:20:33 +0000

    Update menu.php
    Add rudimentary placeholder search system

--- menu.php
+++ menu.php
@@ -16,14 +16,83 @@
         <title>Alexandria Library</title>
     </head>
     <body>
+        <?php
+            //checking to make sure that all form positions are populated
+            if (isset($_POST['title']) && isset($_POST['auth']) && isset($_POST['category']))
+            {
+                
+                $t = $conn -> real_escape_string(htmlentities($_POST['title']));
+                $a = $conn -> real_escape_string(htmlentities($_POST['auth']));
+                $c = $conn -> real_escape_string(htmlentities($_POST['category']));
+
+
+                //creating the sql statements to find books matching users search
+                if ($c == 0)  //if search includes title and author but no category
+                {
+                    $search = "SELECT * FROM books WHERE BookTitle LIKE '%$t%' AND Author LIKE '%$a%' AND Reserve != 'Y'";
+
+                    //storing the row returned by the sql statement
+                    $result1 = $conn->query($search);
+
+                    if ($result1->num_rows  >0)
+                    {
+                        echo "<table border = '1'>";
+                        
+                        //row titles
+                        echo "<tr>";
+                        echo "<td>ISBN</td>";
+                        echo "<td>Book Title</td>";
+                        echo "<td>Author</td>";
+                        echo "<td>Edition</td>";
+                        echo "<td>Release Year</td>";
+                        echo "<td>Category</td>";
+                        echo "<td>Reservation Status</td></tr>";
+
+
+                        //iterating through rows from sql1 query
+                        while($row1 = $result1->fetch_assoc())
+                        {
+                            $id = $conn -> real_escape_string(htmlentities($row1['Category']));
+                            $catFind = "SELECT CategoryDesc FROM category WHERE CategoryID='$id'";
+                            $result2 = $conn->query($catFind);
+
+                            while($row2 = $result2->fetch_assoc())
+                            {
+                                echo "<tr>";
+                                echo "<td>".(htmlentities($row1["ISBN"]))."</td>";
+                                echo "<td>".(htmlentities($row1["BookTitle"]))."</td>";
+                                echo "<td>".(htmlentities($row1["Author"]))."</td>";
+                                echo "<td>".(htmlentities($row1["Edition"]))."</td>";
+                                echo "<td>".(htmlentities($row1["Year"]))."</td>";
+                                echo "<td>".(htmlentities($row2["CategoryDesc"]))."</td>";
+                                echo "</td><td>";
+                                echo ('<a href="add_reserve.php?isbn='.htmlentities($row1["ISBN"]).'">Reserve</a>');
+                                echo "</tr>\n";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        echo "No books matching search found.";
+                    }
+                }
+                
+
+                
+
+            }
+
+        ?>
         <a href="http://localhost/WebD/project/reserve.php">Reserve</a>
         <h2>Search:</h2>
         <form method="post">
-            <label>Title:</label><br>
-            <input type="text" name="title"><br>
-            <label>Author:</label><br>
-            <input type="text" name="auth"><br><br>
-            <select name="category" id="category">
+            <label>Title:</label>
+            <input type="text" name="title">
+            <label>Author:</label>
+            <input type="text" name="auth">
+            <label>Category:</label>
+            <select name="category" name="category">
+            <option value="0">--------------</option>
                 <?php
                     $sql = "SELECT CategoryDesc FROM category";
                     $result = $conn->query($sql);
@@ -40,19 +109,20 @@
                 ?>
             </select>
             <input type="submit" value="Search"/>
+            <br><br>
         </form>
     </body>
     <?php
-            /*
-            - make a search bar
-            - access books via searchbar (select statements with LIKE??)
-            - access categories via dropdown menu
-            - display books matching search (max 5 at a time)
-            - button to reserve each book
-            - logout
-            */
+        /*
+        - make a search bar
+        - access books via searchbar (select statements with LIKE??)
+        - access categories via dropdown menu
+        - display books matching search (max 5 at a time)
+        - button to reserve each book
+        - logout
+        */
 
-            //closing the connection
-            $conn->close();
+        //closing the connection
+        $conn->close();
     ?>
 </html>
