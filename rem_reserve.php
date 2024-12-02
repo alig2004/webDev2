5f9aabd65365b9185c450ba164ae11a7d164a7c3
alig2004 <151762847+alig2004@users.noreply.github.com>
2024-12-02 10:46:41 +0000

    Update rem_reserve.php

--- rem_reserve.php
+++ rem_reserve.php
@@ -62,7 +62,6 @@
 
         //closing the connection
         $conn->close();
-        
     }
 ?>
 <html>
@@ -70,12 +69,16 @@
         <title>Alexandria Library</title>
     </head>
     <body>
-        <a href="http://localhost/WebD/project/menu.php">Menu</a>
+        <header>
+            <h1>Alexandria Library</h1>
+            <navbar>
+                <a href="menu.php">Search</a>
+                <a href="reserve.php">My Reservations</a>
+                <a href="logout.php">Log Out</a>
+            </navbar>
+        </header>
+        <footer>
+            <p><i>Copyright Alison Gleeson, 2024</i></p>
+        </footer>
     </body>
-    <?php
-        /*
-        - link back to menu page
-        - logout
-        */
-    ?>
 </html>
