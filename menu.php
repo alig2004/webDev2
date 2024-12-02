8b57a48801b823fb54095a00bc96679b6d2c0468
alig2004 <151762847+alig2004@users.noreply.github.com>
2024-12-02 10:45:33 +0000

    Update menu.php

--- menu.php
+++ menu.php
@@ -16,8 +16,16 @@
         <title>Alexandria Library</title>
     </head>
     <body>
-    <a href="http://localhost/WebD/project/reserve.php">Reserve</a>
+    <header>
+        <h1>Alexandria Library</h1>
+        <navbar>
+            <a href="menu.php">Search</a>
+            <a href="reserve.php">My Reservations</a>
+            <a href="logout.php">Log Out</a>
+        </navbar>
+    </header>
     <h2>Search:</h2>
+    <p>To see our entire catalogue, press 'Search' without filling in the form.</p>
         <form method="post">
             <label>Title:</label>
             <input type="text" name="title">
@@ -48,141 +56,19 @@
             //checking to make sure that all form positions are populated
             if (isset($_POST['title']) && isset($_POST['auth']) && isset($_POST['category']))
             {
-                
                 $t = $conn -> real_escape_string(htmlentities($_POST['title']));
                 $a = $conn -> real_escape_string(htmlentities($_POST['auth']));
                 $c = $conn -> real_escape_string(htmlentities($_POST['category']));
+                $i = 0;
 
-
-                //creating the sql statements to find books matching users search
-                if ($c == 0)  //if search includes title and author but no category
-                {
-                    $search = "SELECT * FROM books WHERE BookTitle LIKE '%$t%' AND Author LIKE '%$a%'";
-
-                    //storing the row returned by the sql statement
-                    $result1 = $conn->query($search);
-
-                    if ($result1->num_rows  >0)
-                    {
-                        echo "<table border = '1'>";
-                        
-                        //row titles
-                        echo "<tr>";
-                        echo "<td>ISBN</td>";
-                        echo "<td>Book Title</td>";
-                        echo "<td>Author</td>";
-                        echo "<td>Edition</td>";
-                        echo "<td>Release Year</td>";
-                        echo "<td>Category</td>";
-                        echo "<td>Reservation Status</td></tr>";
-
-
-                        //iterating through rows from sql1 query
-                        while($row1 = $result1->fetch_assoc())
-                        {
-                            $id = $conn -> real_escape_string(htmlentities($row1['Category']));
-                            $catFind = "SELECT CategoryDesc FROM category WHERE CategoryID='$id'";
-                            $result2 = $conn->query($catFind);
-
-                            while($row2 = $result2->fetch_assoc())
-                            {
-                                echo "<tr>";
-                                echo "<td>".(htmlentities($row1["ISBN"]))."</td>";
-                                echo "<td>".(htmlentities($row1["BookTitle"]))."</td>";
-                                echo "<td>".(htmlentities($row1["Author"]))."</td>";
-                                echo "<td>".(htmlentities($row1["Edition"]))."</td>";
-                                echo "<td>".(htmlentities($row1["Year"]))."</td>";
-                                echo "<td>".(htmlentities($row2["CategoryDesc"]))."</td>";
-                                echo "</td><td>";
-                                if ($row1["Reserve"] == 'Y')
-                                {
-                                    echo ('Already Reserved');
-                                }
-                                else
-                                {
-                                    echo ('<a href="add_reserve.php?isbn='.htmlentities($row1["ISBN"]).'">Reserve</a>');
-                                }
-                                echo "</tr>\n";
-                            }
-                        }
-                    }
-                    else
-                    {
-                        echo "No books matching search found.";
-                    }
-                }
-                else  //if search includes title and author and category
-                {
-                    $search = "SELECT * FROM books WHERE BookTitle LIKE '%$t%' AND Author LIKE '%$a%' AND Category = '$c'";
-
-                    //storing the row returned by the sql statement
-                    $result1 = $conn->query($search);
-
-                    if ($result1->num_rows  >0)
-                    {
-                        echo "<table border = '1'>";
-                        
-                        //row titles
-                        echo "<tr>";
-                        echo "<td>ISBN</td>";
-                        echo "<td>Book Title</td>";
-                        echo "<td>Author</td>";
-                        echo "<td>Edition</td>";
-                        echo "<td>Release Year</td>";
-                        echo "<td>Category</td>";
-                        echo "<td>Reservation Status</td></tr>";
-
-
-                        //iterating through rows from sql1 query
-                        while($row1 = $result1->fetch_assoc())
-                        {
-                            $id = $conn -> real_escape_string(htmlentities($row1['Category']));
-                            $catFind = "SELECT CategoryDesc FROM category WHERE CategoryID='$id'";
-                            $result2 = $conn->query($catFind);
-
-                            while($row2 = $result2->fetch_assoc())
-                            {
-                                echo "<tr>";
-                                echo "<td>".(htmlentities($row1["ISBN"]))."</td>";
-                                echo "<td>".(htmlentities($row1["BookTitle"]))."</td>";
-                                echo "<td>".(htmlentities($row1["Author"]))."</td>";
-                                echo "<td>".(htmlentities($row1["Edition"]))."</td>";
-                                echo "<td>".(htmlentities($row1["Year"]))."</td>";
-                                echo "<td>".(htmlentities($row2["CategoryDesc"]))."</td>";
-                                echo "</td><td>";
-                                if ($row1["Reserve"] == 'Y')
-                                {
-                                    echo ('Already Reserved');
-                                }
-                                else
-                                {
-                                    echo ('<a href="add_reserve.php?isbn='.htmlentities($row1["ISBN"]).'">Reserve</a>');
-                                }
-                                echo "</tr>\n";
-                            }
-                        }
-                    }
-                    else
-                    {
-                        echo "No books matching search found.";
-                    }
-                }
-
-
+                header("Location: http://localhost/WebD/project/display.php?t=".$t."&a=".$a."&c=".$c."&i=".$i."");
             }
-
         ?>
-        
-        
+        <footer>
+            <p><i>Copyright Alison Gleeson, 2024</i></p>
+        </footer>
     </body>
     <?php
-        /*
-        - display books matching search (max 5 at a time)
-        - button to reserve each book
-        - logout
-        */
-
-        //closing the connection
         $conn->close();
     ?>
 </html>
