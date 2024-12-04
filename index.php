d6ab667a8cd31b2ae44363897b29db7374f6092a
alig2004 <151762847+alig2004@users.noreply.github.com>
2024-12-04 23:53:36 +0000

    Update index.php

--- index.php
+++ index.php
@@ -21,60 +21,6 @@
             <a href="logout.php">Log Out</a>
         </nav>
     </navbar>
-    <?php
-        //connecting to the database
-        require_once "database.php";
-
-        //checking to make sure that all form positions are populated
-        if (isset($_POST['Username']) && isset($_POST['Password']))
-        {
-            
-            $u = $conn -> real_escape_string(htmlentities($_POST['Username']));
-            $p = $conn -> real_escape_string(htmlentities($_POST['Password']));
-
-
-            //checking for username and password in users table
-            if (strlen($p) != 6) //if password is too short
-            {
-                echo 'Password is incorrect length';
-            }
-            else
-            {
-                //creating the sql statement to find username to users table in library database
-                $sql = "SELECT * FROM users WHERE Username = '$u'";
-
-                //storing the row returned by the sql statement
-                $result = $conn->query($sql);
-
-                //if row contains a row, check to see if password matches what was input
-                if ($result->num_rows > 0)
-                {
-                    $row = $result->fetch_assoc();
-
-                    //if password matches, redirect to menu.php
-                    if ($row["Password"] == $p)
-                    {
-                        $_SESSION["account"] = $u;
-                        header("Location: http://localhost/WebD/project/menu.php"); 
-                        exit;
-                    }
-                    else
-                    {
-                        echo 'Incorrect password';
-                    }
-                }
-                else
-                {
-                    echo 'Username not found';
-                }
-
-            }
-
-        }
-
-        //closing the connection
-        $conn->close();
-    ?>
     <div class="login_form">
         <h2>Login:</h2>
         <form method="post">
@@ -86,8 +32,63 @@
         </form>
         <br>
         <a href="register.php">Don't have an account? Click here to register</a>
+        <br>
+        <?php
+            //connecting to the database
+            require_once "database.php";
+
+            //checking to make sure that all form positions are populated
+            if (isset($_POST['Username']) && isset($_POST['Password']))
+            {
+                
+                $u = $conn -> real_escape_string(htmlentities($_POST['Username']));
+                $p = $conn -> real_escape_string(htmlentities($_POST['Password']));
+
+
+                //checking for username and password in users table
+                if (strlen($p) != 6) //if password is too short
+                {
+                    echo '<br><p class="error">Password is incorrect length</p>';
+                }
+                else
+                {
+                    //creating the sql statement to find username to users table in library database
+                    $sql = "SELECT * FROM users WHERE Username = '$u'";
+
+                    //storing the row returned by the sql statement
+                    $result = $conn->query($sql);
+
+                    //if row contains a row, check to see if password matches what was input
+                    if ($result->num_rows > 0)
+                    {
+                        $row = $result->fetch_assoc();
+
+                        //if password matches, redirect to menu.php
+                        if ($row["Password"] == $p)
+                        {
+                            $_SESSION["account"] = $u;
+                            header("Location: http://localhost/WebD/project/menu.php"); 
+                            exit;
+                        }
+                        else
+                        {
+                            echo '<br><p class="error">Incorrect password</p>';
+                        }
+                    }
+                    else
+                    {
+                        echo '<br><p class="error">Username not found</p>';
+                    }
+
+                }
+
+            }
+
+            //closing the connection
+            $conn->close();
+        ?>
     </div>
-    <footer>
+    <footer class="common_footer">
         <p><i>Copyright Alison Gleeson, 2024</i></p>
     </footer>
 </body>
